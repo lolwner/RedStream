0dfe21c8350834074f2966b8c614f2a4f691022d
Victor <lolwner@gmail.com>
2018-12-17 21:02:14 +0200

    Added test sample for youtube video extraction

---
+++ RedStream.YouTubeProviderAPI/Services/VideoExtractorService.cs
@@ -0,0 +1,66 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedStream.YouTubeProviderAPI.Services
+{
+    public class VideoExtractorService
+    {
+        public async Task Run()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { YouTubeService.Scope.YoutubeReadonly },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(this.GetType().ToString())
+                );
+            }
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var channelsListRequest = youtubeService.Channels.List("contentDetails");
+            channelsListRequest.Mine = true;
+            
+            var channelsListResponse = await channelsListRequest.ExecuteAsync();
+
+            foreach (var channel in channelsListResponse.Items)
+            {
+                
+                var uploadsListId = channel.ContentDetails.RelatedPlaylists.Uploads;
+                
+
+                var nextPageToken = "";
+                while (nextPageToken != null)
+                {
+                    var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
+                    playlistItemsListRequest.PlaylistId = uploadsListId;
+                    playlistItemsListRequest.MaxResults = 50;
+                    playlistItemsListRequest.PageToken = nextPageToken;
+                    
+                    var playlistItemsListResponse = await playlistItemsListRequest.ExecuteAsync();
+
+                    foreach (var playlistItem in playlistItemsListResponse.Items)
+                    {
+                        
+                    }
+
+                    nextPageToken = playlistItemsListResponse.NextPageToken;
+                }
+            }
+        }
+    }
+}
+
