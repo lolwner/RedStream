67cbcdd812f06be4044d0f68df92da8ea13ec191
Anuitex <modelerproject@outlook.com>
2019-01-03 14:49:55 +0200

    Added UI for login and register

--- RedStream.WebUI/ClientApp/src/app/app.module.ts
+++ RedStream.WebUI/ClientApp/src/app/app.module.ts
@@ -1,32 +1,69 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './Components/app-header/head.component';
 import { LeftPanelComponent } from './Components/app-left-panel/left-panel.component';
 import { ContainerComponent } from './Components/app-container/container.component';
+import { RouterModule, Routes } from '@angular/router';
+import {MatNativeDateModule,MatDatepickerModule,MatIconModule,MatButtonModule,MatCheckboxModule, MatToolbarModule, 
+  MatCardModule,MatFormFieldModule,MatInputModule,MatRadioModule,MatListModule} from  '@angular/material';
+
+import { LoginComponent } from './Components/login/login.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { HomeComponent } from './Components/home/home.component';
+import { RegisterComponent } from './Components/register/register.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  // { path: 'hero/:id', component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
     LeftPanelComponent,
-    ContainerComponent
+    ContainerComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
     MatInputModule,
     MatFormFieldModule,
     MatSidenavModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatListModule,
     AppRoutingModule
   ],
   providers: [],
