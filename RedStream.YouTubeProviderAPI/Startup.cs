a00462e262c1b59d294ff87aef6e0526b61b72e2
Victor <lolwner@gmail.com>
2019-03-12 00:33:54 +0200

    Updated nuget packages

--- RedStream.YouTubeProviderAPI/Startup.cs
+++ RedStream.YouTubeProviderAPI/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RedStream.DataAccessLayer;
 using RedStream.YouTubeProviderAPI.Wrappers;
 using RedStream.YouTubeProviderAPI.Wrappers.Interfaces;
 
@@ -20,6 +22,7 @@ namespace RedStream.YouTubeProviderAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddMvcCore()
                 .AddAuthorization()
